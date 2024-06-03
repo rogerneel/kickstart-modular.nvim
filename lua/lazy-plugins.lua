5b01b93023999b096bde5ead8d8102735626fe94
Roger Neel <rogerneel@gmail.com>
2024-06-02 22:02:24 -0600

    removing commented custom plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -63,8 +63,6 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
 
-  -- require 'kickstart/plugins/supermaven',
-
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
